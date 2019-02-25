7637de73c37dc9d9e18c7e515bd9fb46e5f7b695
Mandy Kendall <mlkendall93@gmail.com>
2019-02-25 10:27:47 -0500

    Initial Commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `MKendall Creative`,
+    description: `Portfolio Site for Mandy Kendall`,
+    author: `@MLKendall`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
